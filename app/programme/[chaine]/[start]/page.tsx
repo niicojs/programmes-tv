cef4f50cd3e1fd4da485f592500421de42d37673
niico <5653050+niicojs@users.noreply.github.com>
2023-10-30 21:24:52 +0100

    add channel name in details

--- app/programme/[chaine]/[start]/page.tsx
+++ app/programme/[chaine]/[start]/page.tsx
@@ -18,15 +18,18 @@ export default async function Programme({ params }: Props) {
   const chaines = (await kv.get<Chaine[]>('evening')) || [];
   console.log(`chaine ${params.chaine} at ${params.start}`);
 
-  const programme = chaines
-    .filter((c) => c.id === params.chaine)[0]
-    .programmes.filter((p) => p.start === +params.start)[0];
+  const chaine = chaines.filter((c) => c.id === params.chaine)[0];
+  const programme = chaine.programmes.filter(
+    (p) => p.start === +params.start
+  )[0];
 
   return (
     <div className="container mx-auto w-full py-10">
       <Card className="max-w-[650px] mx-auto">
         <CardHeader>
-          <CardTitle>{programme.title}</CardTitle>
+          <CardTitle>
+            {chaine.name} - {programme.title}
+          </CardTitle>
           <CardDescription>
             {programme.category}, de {heure(programme.start)} à{' '}
             {heure(programme.stop)}
